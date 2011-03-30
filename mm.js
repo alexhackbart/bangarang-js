98394bd9fd141914a1468e3c1841e0521749eb9c
Alex Hackbart <alexhackbart@gmail.com>
2011-03-30 15:56:22 -0700

    Initial Commit

---
+++ mm.js
@@ -0,0 +1,37 @@
+var MivaMerchant = {
+	init: function()
+    {
+        // Don't Edit These
+		var mivaScreen = document.body.id;
+        String.prototype.toCamel = function(){
+        	return this.replace(/(\-[a-z])/g, function($1){return $1.toUpperCase().replace('-','');});
+        };        
+        mivaScreen = mivaScreen.toCamel();
+
+		if( MivaMerchant[ mivaScreen ] )
+		{
+			$( document ).ready(function()
+			{ 
+				MivaMerchant[ mivaScreen ](); // If the function exists, run it, otherwise, don't do anything (like Jeff).
+			}); 
+		}
+        
+        // You can start adding global functions here, otherwise just drop them
+        // in on each page. Note that ID's with a hyphen turn into camelCase.
+	},
+	
+	SFNT: function()
+	{
+	   alert( 'Storefront Page Script!' );
+	},
+
+    aboutUs: function() // the id "about-us" becomes "aboutUs"
+    {
+        alert( 'About Us Page Script!' );
+    }
+
+} // end MivaMerchant object
+
+MivaMerchant.init();
+
+// bangarang. 
